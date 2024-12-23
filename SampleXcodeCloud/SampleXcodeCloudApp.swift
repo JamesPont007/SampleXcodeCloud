fe3297685d9741e1ac30e472d75b76368192fbcf
JamesPont007 <nicolasmarchellwork@gmail.com>
2024-12-23 11:34:55 +0200

    Initial Commit

---
+++ SampleXcodeCloud/SampleXcodeCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleXcodeCloudApp.swift
+//  SampleXcodeCloud
+//
+//  Created by Nick on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SampleXcodeCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
